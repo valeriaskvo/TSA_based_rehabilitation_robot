4449fe7121a6b57041d5d984d97ad331e16bc01a
valriaskvo <valriaskvo@gmail.com>
2021-10-28 16:46:32 +0300

    Plot 3d fft

---
+++ Control_and_experiments/Experiment_2_ftt_3dplot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import scipy.interpolate
+from mpl_toolkits.mplot3d import Axes3D
+
+with open('FTT_process.npy', 'rb') as f:
+    x = np.load(f)
+    y = np.load(f)
+    z = np.load(f)
+
+spline = sp.interpolate.Rbf(x,y,z,function='thin-plate')
+
+xi = np.linspace(x.min(), x.max(), 50)
+yi = np.linspace(y.min(), y.max(), 50)
+xi, yi = np.meshgrid(xi, yi)
+
+zi = spline(xi,yi)
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_surface(xi,yi,zi)
+plt.show()
