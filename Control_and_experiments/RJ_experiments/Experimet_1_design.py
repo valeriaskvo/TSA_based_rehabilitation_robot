44de32e338e1996b414a894c435fcae89950b3ab
valriaskvo <valriaskvo@gmail.com>
2022-03-21 22:44:22 +0300

    Fix problems with data saving and rot encoder issue

--- Control_and_experiments/RJ_experiments/Experimet_1_design.py
+++ Control_and_experiments/RJ_experiments/Experimet_1_design.py
@@ -6,21 +6,23 @@ from time import sleep
 import numpy as np
 
 path = "RJ_experiments/Results/"
-filename = "static_experiment_v_const_1"
+filename = "static_experiment_v_const_3"
 
 stand_rj = Stand_RJ()
 stand_rj.get_state()
 
 nominal_velocity = 10
-max_phi = np.deg2rad(90)
+max_phi = np.deg2rad(150)
 
 try:
     while stand_rj.state["phi"] < max_phi:
         stand_rj.motor_set_speed(nominal_velocity)
-        print(stand_rj.state)
-        sleep(0.5)
-
+        
 except KeyboardInterrupt:
     stand_rj.motor_pause()
+
+except Exception as e:
+    print(e)
+
 finally:
     stand_rj.stand_disable(filename=path+filename)
